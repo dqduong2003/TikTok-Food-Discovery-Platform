f759a7dcc2e707ab86b4665fff10d706e305b5a3
Daniel Dang <89473635+dqduong2003@users.noreply.github.com>
2026-01-04 23:13:47 +1100

    fix: only call google places api when place name not null

--- src/index.ts
+++ src/index.ts
@@ -4,16 +4,19 @@ import * as place from "./sample_data.json";
 
 
 async function main() {
-  const sampleVideoPath = "./video/tiktok_cafe_2.mp4"
+  const sampleVideoPath = "./video/multi.mp4"
 
+  // 1. Gemini extracts raw text
   const insightData = await analyzeVideo(sampleVideoPath);
   console.log(insightData);
 
+  // 2. Google Maps verifies it and gets coords
   // const officialPlace = await findPlaceCoordinates(place.restaurant_name, place.address)
-
-  const officialPlace = await findPlaceCoordinates(insightData.restaurant_name, insightData.address)
-  if (officialPlace) {
-    console.log(officialPlace);
+  if (insightData && insightData.restaurant_name) {
+    const officialPlace = await findPlaceCoordinates(insightData.restaurant_name, insightData.address)
+    if (officialPlace) {
+      console.log(officialPlace);
+    }
   }
 }
 
