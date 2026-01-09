1f6952ba59cca4467a3b71671c381cdf36c00934
Quy Duong Dang <dqduong2003@gmail.com>
2026-01-09 22:14:25 +1100

    chore(index): reorder import statements for better organization

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,10 @@
-import app from './app';
 import dotenv from 'dotenv';
 import path from 'path';
 
 // 1. Load Environment Variables (Always do this first)
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+import app from './app';
 
 // 2. Define the Port
 const PORT = process.env.PORT || 4000;
